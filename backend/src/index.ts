243195d8c1c024d993059f0c9546e5e0cde73c26
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-08-31 11:59:57 +0530

    Initialized frontend and backend

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const app: Application = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ origin: "http://localhost:8000" }));
+app.get("/", (req: Request, res: Response) => {
+  res.send("Healthy");
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
