5f4b2c42aac2b15837bf6795aa31a29ff1f9cfe2
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-09-03 22:58:39 +0530

    Added routes and middlewares to the main file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,53 @@
+// Libraries
 import express, { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import dbConnect from "../config/db/dbConnect";
 import * as dotenv from "dotenv";
+
+// Importing dependencies
+import dbConnect from "./config/db/dbConnect";
+import {
+  userRegister,
+  userLogin,
+  fetchAllUsers,
+} from "./controllers/users/user.controller";
 dotenv.config();
+
+// DB configuration
 dbConnect();
+
+// Using Express Server
 const app: Application = express();
+
+// Using Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({ origin: "http://localhost:5000" }));
+
+// Routes(GET, POST, PUT, DELETE)
 app.get("/", (req: Request, res: Response) => {
   res.send("Healthy");
 });
 
+// Get Registration Details
+app.get("/api/users/register", userRegister);
+
+// Get Login Details
+app.get("/api/users/login", userLogin);
+
+// Fetch Registration Details
+app.post("/api/users/register", userRegister);
+
+// Fetch Login Details
+app.post("/api/users/login", userLogin);
+
+// Fetch all users
+app.get("/api/users", fetchAllUsers);
+
+// Server
 const PORT = process.env.PORT || 5000;
 
+// Listening on PORT Number
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
