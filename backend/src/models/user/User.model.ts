159d64fac88debc529f0ca70fd11dded54e4d9e7
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-09-03 23:00:18 +0530

    Added User Model Schema

---
+++ backend/src/models/user/User.model.ts
@@ -0,0 +1,63 @@
+import { Schema, model } from "mongoose";
+import { User } from "../../interfaces/User";
+
+const UserSchema = new Schema(
+  {
+    firstName: { type: String, required: [true, "First Name is required"] },
+    lastName: { type: String, required: [true, "Last Name is required"] },
+    profilePhoto: {
+      type: String,
+      default:
+        "https://pixabay.com/vectors/blank-profile-picture-mystery-man-973460/",
+    },
+    email: { type: String, required: [true, "Email is required"] },
+    bio: { type: String },
+    password: { type: String, required: [true, "password is required"] },
+    postCount: { type: Number, default: 0 },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: { type: Boolean, default: false },
+    role: { type: String, enum: ["Admin", "Guest", "Blogger"] },
+    isFollowing: { type: Boolean, default: false },
+    isUnFollowing: { type: Boolean, default: false },
+    isAccountVerified: { type: Boolean, default: false },
+    accountVerificationToken: String,
+    accountVerificationTokenExpires: Date,
+    viewBy: {
+      type: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+        },
+      ],
+    },
+    followers: {
+      type: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+        },
+      ],
+    },
+    passwordChangeAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+    active: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+    timestamps: true
+  }
+);
+
+export default model<User>("User", UserSchema);
