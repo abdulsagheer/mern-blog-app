9ad3393a7fa1b1fa7f933d4c8270e5bbf06b5a09
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-09-13 22:04:05 +0530

    Added register route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
-import Home from "./components/Home/Home";
+// Importing Libraries
 import { Routes, Route } from "react-router-dom";
+
+// Importing Dependencies
+import Home from "./components/Home/Home";
+import Register from "./components/Users/Register/Register";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
