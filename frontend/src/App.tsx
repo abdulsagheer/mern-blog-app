8df2a0068963e2a31ebed882fc21554e531622f6
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-09-14 21:28:35 +0530

    Added Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,19 @@
 import { Routes, Route } from "react-router-dom";
 
 // Importing Dependencies
-import Home from "./components/Home/Home";
-import Register from "./components/Users/Register/Register";
+import { Home } from "./components/Home/Home";
+import Navbar from "./components/Navigation/Navbar";
+import { Login } from "./components/Users/Login/Login";
+import { Register } from "./components/Users/Register/Register";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
